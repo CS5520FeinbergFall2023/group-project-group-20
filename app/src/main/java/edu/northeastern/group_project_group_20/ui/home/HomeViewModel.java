1fe94758960fd30d913420f7fe6cad9c7a7da1a4
alanleng <leng.hou@northeastern.edu>
2023-11-17 08:45:46 -0800

    init project

---
+++ app/src/main/java/edu/northeastern/group_project_group_20/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package edu.northeastern.group_project_group_20.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
