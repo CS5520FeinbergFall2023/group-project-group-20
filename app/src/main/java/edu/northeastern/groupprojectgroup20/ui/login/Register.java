cadbf7a8b137159066d40aebb721673638ad16bd
alanleng <leng.hou@northeastern.edu>
2023-11-18 17:18:38 -0800

    init login and register page

--- app/src/main/java/edu/northeastern/groupprojectgroup20/ui/login/Register.java
+++ app/src/main/java/edu/northeastern/groupprojectgroup20/ui/login/Register.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -18,10 +17,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import edu.northeastern.groupprojectgroup20.R;
-import edu.northeastern.groupprojectgroup20.ui.loginn.LoginActivity;
 
 public class Register extends AppCompatActivity {
 
