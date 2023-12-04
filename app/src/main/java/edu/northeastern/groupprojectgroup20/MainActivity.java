479bb77a5be168669634a01d78f800de1239e306
alanleng <leng.hou@northeastern.edu>
2023-12-03 18:26:13 -0800

    add refresh

--- app/src/main/java/edu/northeastern/groupprojectgroup20/MainActivity.java
+++ app/src/main/java/edu/northeastern/groupprojectgroup20/MainActivity.java
@@ -113,8 +113,10 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // To email app in the new window
+                startActivity(intent);
             }
         });
 
